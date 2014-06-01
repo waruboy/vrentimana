05fb4ff9ced011abc0b38f8b569464d637523f28
Taufiq Muhammadi <waruboy@yahoo.com>
2014-06-01 08:33:39 +0700

    now name is required

--- app/models/tj_stop.rb
+++ app/models/tj_stop.rb
@@ -1,5 +1,6 @@
 class TjStop < ActiveRecord::Base
   validates :google_lookup, presence: true
+  validates :name, presence: true
   geocoded_by :google_lookup
   after_validation :geocode, if: :google_lookup_changed?
 end
