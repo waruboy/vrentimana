94db25921012861dd18c76fa4fd5ea7b19e58cd6
Taufiq Muhammadi <waruboy@yahoo.com>
2014-06-07 08:46:02 +0700

    disable back_office route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/back_office', as: 'rails_admin'
+  # mount RailsAdmin::Engine => '/back_office', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
